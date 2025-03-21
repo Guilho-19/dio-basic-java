9aae291d4046478fe8496845257bd7accbd9344b
Guilho-19 <guilhermepalaronunessilva@gmail.com>
2025-03-21 16:03:46 -0300

    Comentários

---
+++ Aulas/aulaIDE/src/aulaIDE/Comentarios.java
@@ -0,0 +1,23 @@
+package aulaIDE;
+
+public class Comentarios {
+	public static void main (String[] args) {
+		// Olá, eu sou um comentário em uma única linha
+		
+		/* Olá
+		 * Eu sou um comentário
+		 * que pode ser mais detalhado
+		 * quando necessário
+		 */
+	}
+	
+	/**
+	 * Estas duas estrelinhas acima
+	 * é para identificar que você
+	 * pretende elaborar um comentário
+	 * a nível de documentação
+	 */
+	public void metodo() {
+		System.out.println("Comentários...");
+	}
+}
