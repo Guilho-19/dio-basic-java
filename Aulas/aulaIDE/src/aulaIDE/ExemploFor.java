bff0ed0b99fa5c5a00a44e40fef46e1b09ee4b36
Guilho-19 <guilhermepalaronunessilva@gmail.com>
2025-04-21 15:43:43 -0300

    Estruturas de Repetição

---
+++ Aulas/aulaIDE/src/aulaIDE/ExemploFor.java
@@ -0,0 +1,11 @@
+package aulaIDE;
+
+public class ExemploFor {
+	public static void main(String[] args) {
+		for (int carneirinhos = 1; carneirinhos <= 200; carneirinhos ++) {
+			System.out.println("Contando carneirinhos: " + carneirinhos);
+		}
+		
+		System.out.println("Joãozinho dormiu...");
+	}
+}
